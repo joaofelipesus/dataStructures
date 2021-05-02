77c31104ab1e8a6d4c61fc6a06806eab1fd23060
Joao Felipe <joaofelipe1294@gmail.com>
2021-05-02 10:34:47 -0300

    Added List class and add methdo description.

---
+++ src/main/java/br/com/lopes/dataStructures/list/List.java
@@ -0,0 +1,20 @@
+package br.com.lopes.dataStructures.list;
+
+public class List {
+	private Integer[] elements = new Integer[0];
+	private Integer size = 0;
+	
+	/*
+	 * Adds a value on the end of the list.
+	 * 
+	 * @param value integer value that will be added on the list
+	 * */
+	public void add(Integer value) {
+		
+	}
+	
+	public Integer getSize() {
+		return size;
+	}
+
+}
