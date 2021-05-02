37b5bc4f8119ef76ee44e81371370795f8461a9f
Joao Felipe <joaofelipe1294@gmail.com>
2021-05-02 11:17:13 -0300

    Refactored add method.

--- src/main/java/br/com/lopes/dataStructures/list/List.java
+++ src/main/java/br/com/lopes/dataStructures/list/List.java
@@ -10,18 +10,29 @@ public class List {
 	 * @param value integer value that will be added on the list
 	 * */
 	public void add(Integer value) {
-		
-		Integer[] newElements = new Integer[this.size + 1];
-		for(int index = 0; index < this.size; index++) {
-			newElements[index] = this.elements[index];
-		}
-		newElements[size] = value;
-		this.elements = newElements;
-		this.size += 1;
+		increateArraySize();
+		this.elements[size] = value;
+		increaseSize();
 	}
 	
 	public Integer size() {
 		return size;
+	}
+	
+	/*
+	 * Increase current elements array, and set this.elements an array
+	 * with an extra empty position.
+	 */
+	private void increateArraySize() {
+		Integer[] increasedSizeArray = new Integer[this.size + 1];
+		for(int index = 0; index < this.size; index++) {
+			increasedSizeArray[index] = this.elements[index];
+		}
+		this.elements = increasedSizeArray;
+	}
+	
+	private void increaseSize() {
+		this.size += 1;
 	}
 
 }
