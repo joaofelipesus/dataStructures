927473af3a6daf0a2dafc35146f19b674bf7fcb2
Joao Felipe <joaofelipe1294@gmail.com>
2021-05-03 08:00:29 -0300

    Added Stack class method descriptions.

---
+++ src/main/java/br/com/lopes/dataStructures/stack/Stack.java
@@ -0,0 +1,40 @@
+package br.com.lopes.dataStructures.stack;
+
+public class Stack {
+
+	/*
+	 * Adds a new element to the top of the stack.
+	 */
+	public void push() {}
+	
+	
+	/*
+	 * Return element from top and remove it.
+	 * 
+	 * @throws IndexOutOfBoundsException when stack doesn't have any element
+	 */
+	public Integer pop() {
+		return 0;
+	}
+	
+	/*
+	 * Check if stack contains element
+	 * 
+	 * @returns boolean: return true if stack have at least one element otherwise 
+	 * return false.
+	 */
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	/*
+	 * Return stack top element.
+	 * 
+	 * @return Integer value from stack top element.
+	 * 
+	 * @throws IndexOutOfBoundsException when stack doesn't have any element
+	 */
+	public Integer getTop() {
+		return 0;
+	}
+}
